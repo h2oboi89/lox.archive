0bf1d9dd5ce5e263d0bd9421bbd3be2caa57c365
Waters <h2oboi89@gmail.com>
2019-11-24 21:58:54 -0500

    Added test for clock() built in, refactored tests
    - tests now broken up into partial class files

---
+++ UnitTests/LoxFramework/InterpreterTests_Globals.cs
@@ -0,0 +1,20 @@
+﻿using LoxFramework;
+using NUnit.Framework;
+
+namespace UnitTests.LoxFramework
+{
+    [TestFixture]
+    public partial class InterpreterTests
+    {
+        [Test]
+        public void Clock()
+        {
+            Interpreter.Run("print clock();");
+
+            Assert.That(Errors, Is.Empty);
+
+            Assert.That(Results.Count, Is.EqualTo(1));
+            Assert.That(Results[0], Does.Match(@"-?\d+(?:\.\d+)?"));
+        }
+    }
+}
