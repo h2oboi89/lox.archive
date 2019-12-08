925f0da7099bafbe94314edb278d7008f6637c8c
Waters <h2oboi89@gmail.com>
2019-12-08 13:18:59 -0500

    extracted Scanner into it's own DLL project

---
+++ Scanning/ScanError.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Scanning
+{
+    public class ScanError
+    {
+        public int Line { get; private set; }
+
+        public string Message { get; private set; }
+
+        public ScanError(int line, string message)
+        {
+            Line = line;
+            Message = message;
+        }
+    }
+}
